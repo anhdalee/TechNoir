81c6f9230c539fd20728bdaa40847b5b8f8e0a64
anhdalee <danh.alee13@gmail.com>
2024-09-19 16:57:23 +0700

    commit base, reset css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Hello Anh Em đây là dự án thương mại điện tử công nghệ số</h1>
-      <h2>Hãy thiết lập giao diện thương mại điện tử như shoppee</h2>
+      <h1>Chào tất cả anh em </h1>
     </div>
   )
 }
