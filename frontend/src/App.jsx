cd58381365a4b4fadfef41bc70fac7c62730570f
anhdalee <danh.alee13@gmail.com>
2024-09-19 09:58:31 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Anh Em đây là dự án thương mại điện tử công nghệ số</h1>
+    </div>
+  )
+}
+
+export default App
