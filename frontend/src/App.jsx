59b3a20f4fa08b093284c8d4c2a42869907b61a2
anhdalee <danh.alee13@gmail.com>
2024-09-19 10:03:18 +0700

    add app h2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello Anh Em đây là dự án thương mại điện tử công nghệ số</h1>
+      <h2>Hãy thiết lập giao diện thương mại điện tử như shoppee</h2>
     </div>
   )
 }
